2d840da97f416f312632ab942bd4e35ab5950e61
Marko Leppänen <anterolep@gmail.com>
2019-12-04 14:43:15 +0200

    day 4 in

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 ## day1
 Rust
-
 `cargo run data.txt`
 
 
 ## day2
 Rust
-
 `cargo run`
+
+
+## day3
+Rust
+`cargo run`
+
+# day4
+Nodejs, Sorry for the regex solution, I was in a hurry :(
+node src/main.js
---
+++ day4/src/main.js
@@ -0,0 +1,23 @@
+{
+  const regex1 = /(.)\1{1,}/g; //find pairs+
+  const regex2 = /^1*2*3*4*5*6*7*8*9*$/g; //incremental
+  const regex3 = /^(1{0,})(2{0,})(3{0,})(4{0,})(5{0,})(6{0,})(7{0,})(8{0,})(9{0,})$/g; //find sequences of same number
+  let current = 138241;
+  const to = 674034;
+
+  let count = 0;
+  let count2 = 0;
+  let m;
+  while (current++ <= to) {
+    if (String(current).match(regex1) && String(current).match(regex2)) {
+      count++;
+      while ((m = regex3.exec(current)) !== null) {
+        if (m.some(m => m.length === 2)) {
+          count2++;
+        }
+      }
+    }
+  }
+  console.log("part 1 solution", count);
+  console.log("part 2 solution", count2);
+}
